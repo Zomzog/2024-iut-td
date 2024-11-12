c03ae697b546ba6fa34c02e983eb14184a2634b0
Zomzog <zomzog@gmail.com>
2024-11-12 12:00:44 +0100

    clean

--- exo10/src/main/kotlin/iut/nantes/Database.kt
+++ exo10/src/main/kotlin/iut/nantes/Database.kt
@@ -4,20 +4,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class Database {
-    private val movies = mutableMapOf<String, Movie>().also { m ->
-        MOVIES.forEach{ m[it.name] = it }
-    }
+    private val movies = mutableMapOf<String, Movie>()
 
-    fun addMovie(movie: Movie) {
-        if (movies.containsKey(movie.name)) {
-            throw IllegalArgumentException("Movie already exists")
-        }
-        movies[movie.name] = movie
-    }
-
-    fun listMovies() = movies.values.toList()
-    fun getMovie(name: String): Movie? {
-        return movies[name]
-    }
 
 }
